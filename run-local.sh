fd624dd91a8d63763be8072c6b04e6fba729d50b
Prince Wang <princewang1994@gmail.com>
2018-09-07 21:04:08 +0800

    sslocal modify

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,12 @@
 export CUR_DIR=$(cd "$(dirname "$0")"; pwd)
+set -e
+
+local_conf=$1
+
+if [ $local_conf ]; then
+	cp -v $local_conf $CUR_DIR/sslocal.conf
+fi
+
 docker run \
 	--name sslocal \
 	--env mode="local" \
